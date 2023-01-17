fac64577ca3aae88a11d0f322ab8be5132b101de
ScarFace424 <120308855+ScarFace424@users.noreply.github.com>
2023-01-17 13:40:33 +0100

    Read in is working, but try with WAV

--- src/com/sprunk/projects/musicapplication/Main.java
+++ src/com/sprunk/projects/musicapplication/Main.java
@@ -1,27 +1,24 @@
 package com.sprunk.projects.musicapplication;
 
-import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import com.sprunk.projects.musicapplication.display.UserInterface;
 import com.sprunk.projects.musicapplication.playmusic.MusicPlayer;
 import com.sprunk.projects.musicapplication.filereading.Filter;
-import com.sprunk.projects.musicapplication.filereading.FileReader;
+import com.sprunk.projects.musicapplication.filereading.AudioFileReader;
 import com.sprunk.projects.musicapplication.exception.AudioReadException;
 
 public class Main {
     static final String TITLE = "Music Application";
 	static final String MUSIC_FILE = "Nightcore - Floating In Love";
-	static final String FILE_PATH = "/~/projects/Music-Application/musicFiles/";
+	//static final String FILE_PATH = "/~/projects/MusicApplication";
 
 	public static void main(String[] args) {
-		File file = new File("");
-		//String absolutePath = file.getAbsolutePath();
 		Filter filter = new Filter(MUSIC_FILE);
-		UserInterface.Frame(TITLE, MUSIC_FILE);
-		FileReader fileReader = new FileReader();
+		//UserInterface.Frame(TITLE, MUSIC_FILE);
+		AudioFileReader audioFileReader = new AudioFileReader();
 		AudioInputStream audio = null;
 		try {
-			audio = fileReader.readFile(MUSIC_FILE, FILE_PATH);
+			audio = audioFileReader.readFile(MUSIC_FILE);
 		} catch(AudioReadException are) {
 			System.out.println(are.getMessage());
 		}
--- src/com/sprunk/projects/musicapplication/filereading/AudioFileReader.java
+++ src/com/sprunk/projects/musicapplication/filereading/AudioFileReader.java
@@ -1,17 +1,25 @@
 package com.sprunk.projects.musicapplication.filereading;
 
-import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.io.InputStream;
 import java.io.IOException;
+import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import com.sprunk.projects.musicapplication.exception.AudioReadException;
 
-public class FileReader {
-    static final int BIT_RATE = 16;
+public class AudioFileReader {          //TODO MP3 Files cant be played, find a solution 
+    static final int BIT_RATE = 16;     //WAV files are accepted, but bigger
 
-    public AudioInputStream readFile(String fileName, String filePath) throws AudioReadException {
+    public AudioInputStream readFile(String fileName) throws AudioReadException {
+        Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
+        File file = new File(path + "/musicFiles/" + fileName + ".mp3");    //
+        
+        /*
         String pathWithFile = filePath + fileName;
         InputStream fileStream = null;
         try {
@@ -19,11 +27,12 @@ public class FileReader {
         } catch(AudioReadException are) {
             System.out.println(are.getMessage());
         }
-        
+        */
+
         AudioInputStream in = null;
         AudioInputStream out = null;
         try {
-            in = AudioSystem.getAudioInputStream(fileStream);
+            in = AudioSystem.getAudioInputStream(file);
             AudioFormat baseFormat = in.getFormat();
             AudioFormat decodedFormat = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 
                                                     baseFormat.getSampleRate(),
@@ -34,10 +43,12 @@ public class FileReader {
                                                     false);
 
             out = AudioSystem.getAudioInputStream(decodedFormat, in);
-        } catch (Exception e) {
+        } catch (UnsupportedAudioFileException e) {
+            throw new AudioReadException("AudioFile not supported!");
+        } catch(IOException io) {
             throw new AudioReadException("Failed to interpret AudioFile!");
         } finally {
-            if (in != null) { 
+            if (in != null || out != null) { 
             in = null;
             out = null;
             }
@@ -45,8 +56,9 @@ public class FileReader {
 
         return out;
     }
-
+    /*
     private InputStream getFileAsStream(String pathWithFile) throws AudioReadException {
+        //ClassLoader cl = this.getClass().getClassLoader();
         InputStream stream = null;
         try {
             stream = getClass().getResourceAsStream(pathWithFile);
@@ -55,11 +67,16 @@ public class FileReader {
             throw new AudioReadException("Failed to read AudioFile!");
         } finally {
             try {
-                if(stream != null)
-                stream.close();
+                if(stream != null) {
+                    stream.close();
+                } else {
+                    throw new AudioReadException("The FileStream is empty!");
+                }
+                
             } catch(IOException io) {
                 throw new AudioReadException("Failed to close InputStream!");
             }
         }
     }
+    */
 }
