f191d399f8b06ca0747332473de996a4a96cbe7c
ScarFace424 <120308855+ScarFace424@users.noreply.github.com>
2023-02-11 02:07:19 +0100

    Add code in classes

--- src/com/sprunk/projects/musicapplication/Main.java
+++ src/com/sprunk/projects/musicapplication/Main.java
@@ -1,6 +1,7 @@
 package com.sprunk.projects.musicapplication;
 
 import java.io.File;
+import java.util.Scanner;
 import javax.sound.sampled.AudioInputStream;
 import com.sprunk.projects.musicapplication.display.UserInterface;
 import com.sprunk.projects.musicapplication.playmusic.MusicPlayer;
@@ -11,7 +12,7 @@ public class Main {
     static final String TITLE = "Music Application";
 	static final String MUSIC_FILE = "Nightcore - Floating In Love";
 	static final String FILE_PATH = "/musicFiles/";
-
+	/*
 	public static void main(String[] args) {
 		File file = new File("");
 		String absolutePath = file.getAbsolutePath();
@@ -20,4 +21,32 @@ public class Main {
 		FileReader fileReader = new FileReader();
 		AudioInputStream audio = fileReader.readFile(MUSIC_FILE, absolutePath + FILE_PATH);
 	}
+	*/
+
+	public static void main(String[] args) {
+        try {
+            String filePath = FILE_PATH + MUSIC_FILE;
+            MusicPlayer player = new MusicPlayer();
+            audioPlayer.play();
+            Scanner sc = new Scanner(System.in);
+            while (true) {
+                System.out.println("1. pause");
+                System.out.println("2. resume");
+                System.out.println("3. restart");
+                System.out.println("4. stop");
+                System.out.println("5. Jump to specific time");
+                int c = sc.nextInt();
+                audioPlayer.Choice.gotoChoice(c);
+                if (c == 4)
+                break;
+            }
+
+            sc.close();
+        } 
+          
+        catch (Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
+    }
 }
--- src/com/sprunk/projects/musicapplication/playmusic/MusicPlayer.java
+++ src/com/sprunk/projects/musicapplication/playmusic/MusicPlayer.java
@@ -1,5 +1,64 @@
 package com.sprunk.projects.musicapplication.playmusic;
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.Clip;
+import com.sprunk.projects.musicapplication.options.AudioOptions;
+
 public class MusicPlayer {
-    
+      
+    // to store current position
+    Long currentFrame;
+    Clip clip;
+      
+    // current status of clip
+    //String status;
+      
+    AudioInputStream audioInputStream;
+  
+    // constructor to initialize streams and clip
+    public MusicPlayer() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        // create AudioInputStream object
+        audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+          
+        // create clip reference
+        clip = AudioSystem.getClip();
+          
+        // open audioInputStream to the clip
+        clip.open(audioInputStream);
+          
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void gotoChoice(int c) throws IOException, LineUnavailableException, UnsupportedAudioFileException {
+        switch (c) 
+        {
+            case 1:
+                AudioOptions.pause();
+                break;
+            case 2:
+                AudioOptions.resumeAudio();
+                break;
+            case 3:
+                AudioOptions.restart();
+                break;
+            case 4:
+                AudioOptions.stop();
+                break;
+            case 5:
+                System.out.println("Enter time (" + 0 + 
+                ", " + clip.getMicrosecondLength() + ")");
+                Scanner sc = new Scanner(System.in);
+                long c1 = sc.nextLong();
+                AudioOptions.jump(c1);
+                break;
+      
+        }
+      
+    }
 }
